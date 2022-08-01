4d5de452bdad5747307c6720761f4f4506660755
madhushikahansani <madhuhansi920@gmail.com>
2022-08-01 17:21:37 +1000

    Update Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Software Development </h1>
+                    <p>
+                     sdsadasdasdsd sdsadasdsaddasd csadasdsadsadsa adasdasdsadasdsad adssadsadasdasdasd adsadas aasdas adas adasdsa asd sdasasds   
+                    </p>
+            </div>
+        )
+    }
+}
+export default Home;
